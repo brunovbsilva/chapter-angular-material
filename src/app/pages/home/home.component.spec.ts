b7aaa49698a6bd1574d6fe2b76c32a333e0ad96f
Bruno Silva <brunovinicius0704@gmail.com>
2023-05-28 15:08:38 -0300

    ajuste testes unitários

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +10,13 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        HomeComponent
+      ],
+      imports: [
+        MatDividerModule,
+        MatIconModule
+      ]
     })
     .compileComponents();
 
