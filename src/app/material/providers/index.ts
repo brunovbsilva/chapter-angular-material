f959f277990c55d9aba2228f7d9b04910e6f9a5c
Bruno Silva <brunovinicius0704@gmail.com>
2023-05-31 15:08:47 -0300

    start

--- src/app/material/providers/index.ts
+++ src/app/material/providers/index.ts
@@ -1,12 +1,16 @@
+import { MAT_CHECKBOX_DEFAULT_OPTIONS } from "@angular/material/checkbox";
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from "@angular/material/form-field";
+import { MAT_SLIDE_TOGGLE_DEFAULT_OPTIONS } from "@angular/material/slide-toggle";
 
 const matFormFieldDefaultOptions: MatFormFieldDefaultOptions = {
     appearance: 'outline',
     color: 'primary',
-    floatLabel: 'always',
+    floatLabel: 'auto',
     subscriptSizing: 'fixed'
-}
+};
 
 export const MaterialProviders = [
+    // { provide: MAT_CHECKBOX_DEFAULT_OPTIONS, useValue: { color: 'primary' }},
+    // { provide: MAT_SLIDE_TOGGLE_DEFAULT_OPTIONS, useValue: { color: 'primary' }},
     // { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: matFormFieldDefaultOptions },
 ]
